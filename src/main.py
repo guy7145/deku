76eaaf0b94216f214f157c7e42577f5367ee9f07
guydani <guy7145@gmail.com>
2018-01-06 18:14:35 +0200

    refactoring? just needed to push last commit.

--- src/main.py
+++ src/main.py
@@ -6,9 +6,6 @@ def main():
     download_path = 'D:\_Guy\d9anime\downloaded'
     anime_name = 'one punch man'
     eps = episodes(1, 2)
-    # print(search(anime_name))
-    # print(find_series_url_by_name(anime_name))
-    # print(debug_src(anime_name))
     download(anime_name, eps, server=RapidVideo)
     return
 
